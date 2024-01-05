a66bb8ee07976f88c9a227fc31fba1991bb35763
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-01-05 19:59:39 +0600

    backend done

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { getUser, createUser } = require('../controllers/userController');
-const router=express.Router();
+const { getUser, createUser, getOneUser, deleteUser, updateUser } = require('../controllers/userController');
 
-router.get('/user',getUser)
-router.post('/user',createUser)
-module.exports=router
+const router = express.Router();
+
+router.get('/user', getUser);
+router.get('/user/:id', getOneUser);
+router.post('/user', createUser);
+router.delete('/user/:id',deleteUser);
+router.put('/user/:id',updateUser)
+module.exports = router;
